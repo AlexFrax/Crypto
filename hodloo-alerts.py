2d3e5a6d5b41f65699d86ada1cd6496698e0583f
NobbisCrypto <90933537+NobbisCrypto@users.noreply.github.com>
2021-09-21 05:06:05 +0200

    Update hodloo-alerts.py

--- hodloo-alerts.py
+++ hodloo-alerts.py
@@ -1,12 +1,23 @@
-#!/usr/bin/python3
 import requests
 from telethon import TelegramClient, events, sync
 
-# Remember to use your own values from my.telegram.org!
+###
+### Variables
+###
 api_id = 
 api_hash = ''
-client = TelegramClient('alerts', api_id, api_hash)
+binance_10_webhook = ''
+binance_5_webhook = ''
+
+# Only include USDT pairs but exclude anything with "DOWN/" or "UP/" in the name aka Binance Leveraged Tokens.
+# Valid pair examples: ADA/USDT, LINK/USDT and so on
+# Non-valid pairs: ADADOWN/USDT, ADA/BUSD, LINK/ETH, LINK/BTC and so on
 pattern = '(?i)^(?!.*DOWN\/)(?!.*UP\/).*\/USDT.*'
+
+###
+### Main script
+###
+client = TelegramClient('alerts', api_id, api_hash)
 
 def SendToDiscord(ticker,url):
     ticker = ticker.split("\n")[0]
@@ -15,12 +26,12 @@ def SendToDiscord(ticker,url):
 
 @client.on(events.NewMessage(chats='Hodloo Binance 10%',pattern=pattern))
 async def binance10_event_handler(event):
-    url = ""
+    url = binance_10_webhook
     SendToDiscord(event.raw_text, url)
 
 @client.on(events.NewMessage(chats='Hodloo Binance 5%',pattern=pattern))
 async def binance5_event_handler(event):
-    url = ""
+    url = binance_5_webhook
     SendToDiscord(event.raw_text, url)
 
 client.start()
